6013df2820fc832da0cf6c2a140c7d72ca9edf87
StatisticianLeboo <lebooian61@gmail.com>
2023-02-21 10:19:07 -0800

    j

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - to check if alphabet is lower case
+ *
+ * @c: parameter that will be checked
+ *
+ * Return: 1 if lower case
+ * and 0 otherwise
+ */
+
+int _islower(int c);
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
