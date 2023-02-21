518bd15ca3f0327060146bb1255b41caac6aa8b7
StatisticianLeboo <lebooian61@gmail.com>
2023-02-21 08:34:24 -0800

    alphabets in small case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- * main - main code
  * print_alphabet - function to  prins the alphabets in small case
  * Return: always 0;
  */
@@ -11,6 +10,8 @@ void print_alphabet(void)
 	char lda;
 
 	for (lda = 'a'; lda <= 'z'; lda++)
+	{
 		_putchar(lda);
-	_putchar("\n");
+	}
+	_putchar('\n');
 }
