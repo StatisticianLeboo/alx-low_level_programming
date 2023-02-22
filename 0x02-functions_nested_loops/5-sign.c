8f00e17c0075ff90bb0d8000311c1d580288f096
StatisticianLeboo <lebooian61@gmail.com>
2023-02-21 18:52:24 -0800

    3 and 4

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a cgaracter
+ * @n: parameter in question
+ * Return: 1 for positive, -1 for negative and 0 for 0
+ */
+
+int print_sign(int n);
+
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
