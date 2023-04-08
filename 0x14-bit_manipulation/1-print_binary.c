8dffa43f75312d3883ebb67963b88c91a01baa24
StatisticianLeboo <lebooian61@gmail.com>
2023-04-08 01:15:12 -0700

    qn1 print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * print_binary - will convert number to binary
- * @n: the variable
+ * print_binary - prints the binary representation of a number.
+ * @n:  number to convert binary
  * Return: void
  */
 
-void print_binary(unsigned long int n) 
+void print_binary(unsigned long int n)
 {
-	if (n > 1)
-	{
-		print_binary(n >> 1);
-	}
-	putchar((n & 1) ? '1' : '0');
+	if (n == 0)
+		_putchar('0');
+	else
+		print_num_binary(n);
 }
 
 /**
