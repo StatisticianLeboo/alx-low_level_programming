b05955de3e6b41af745d4274eaaeae9816a8471e
StatisticianLeboo <lebooian61@gmail.com>
2023-04-08 02:03:19 -0700

    qn 2 and 3

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * get_bit - will extract the bit value
- * @n: is the variable
- * @index: is the index value
+ * get_bit - will give value of a bit at a given index
+ * @n: variable to check the value of
+ * @index: The index to look for the number
+ * Return: A value at a given index or -1 if an error occoured
  */
-
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
@@ -13,7 +13,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (index > (sizeof(8) * 8))
 		return (-1);
 
-	/*multiple loops to iterate number to the right index*/
+	/*this loop will run until the number to the right index*/
 	while (i < index)
 	{
 		n = n >> 1;
