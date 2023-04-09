f6f55b66989e8b646b4e88d387aa2c3fd12d3d5f
StatisticianLeboo <lebooian61@gmail.com>
2023-04-09 04:16:19 -0700

    qn 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,43 +9,42 @@
  *
  * Return: The number of letters read and printed, or 0 if an error occurred
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd, bytes_read, bytes_written;
-    char *buf;
+	int fd, bytes_read, bytes_written;
+	char *buf;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-    buf = malloc(sizeof(char) * letters);
-    if (buf == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-    bytes_read = read(fd, buf, letters);
-    if (bytes_read == -1)
-    {
-        close(fd);
-        free(buf);
-        return (0);
-    }
+	bytes_read = read(fd, buf, letters);
+	if (bytes_read == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
 
-    bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
-    {
-        close(fd);
-        free(buf);
-        return (0);
-    }
-
-    close(fd);
-    free(buf);
-    return (bytes_written);
+	close(fd);
+	free(buf);
+	return (bytes_written);
 }
-
